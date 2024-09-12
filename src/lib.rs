1f137a173708c566c9e7a4bdc7c77cffa7c05700
Its <2779546+Its-its@users.noreply.github.com>
2024-09-12 01:40:02 -0700

    Organization

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
+mod id;
 mod registration;
 mod response;
+mod structs;
 
+pub use id::*;
 pub use registration::*;
 pub use response::*;
--- src/registration.rs
+++ src/registration.rs
@@ -1,6 +1,24 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+use crate::{
+    structs::{MemberPartial, WebsitePartial},
+    MemberUuid, WebsiteUuid,
+};
 
 // Install
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RegisterNewJson {
+    pub instance_id: Uuid,
+
+    pub website_id: WebsiteUuid,
+    pub owner_id: MemberUuid,
+
+    pub member: MemberPartial,
+    pub website: WebsitePartial,
+}
 
 #[derive(Serialize, Deserialize)]
 pub enum InstallResponse {
