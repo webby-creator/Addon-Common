cb3c4c936dac47e9431e46a2bca33e3ff07ef362
Its <2779546+Its-its@users.noreply.github.com>
2024-09-12 18:34:26 -0700

    Global Declaration

---
+++ ts/index.d.ts
@@ -0,0 +1,11 @@
+/**
+ * The URL of the REST API.
+ *
+ * Example: https://api.example.com/addon-api/v1/{addon-id}/
+ */
+const API_URL: string;
+
+/**
+ * The UUID of the addon instance.
+ */
+const INSTANCE_UUID: string;
